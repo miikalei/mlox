495ecd77cb979f19a0646ab121c1edf1ef9cf37c
Miika Leinonen <miika@uprightproject.com>
2024-10-26 22:23:29 +0300

    Set up file or line input reading

--- src/mlox.ts
+++ src/mlox.ts
@@ -1,6 +1,46 @@
-export function main() {
-  console.log("I'm running!")
-  return 6
+import fs from 'fs'
+import rl from 'readline'
+
+export async function main() {
+  const args = process.argv.slice(2);
+    if (args.length > 1) {
+      console.log("Usage: mlox [script]")
+      process.exit(64)
+    } else if (args.length === 1) {
+      runFile(args[0]);
+    } else {
+      await runPrompt();
+    }
 }
 
-main();
+function runFile(path: string) {
+  const fileContent = fs.readFileSync(path).toString();
+  run(fileContent);
+}
+
+async function runPrompt() {
+  const io = rl.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  })
+  io.setPrompt('€');
+  async function getLine(): Promise<string> {
+    io.prompt();
+    return new Promise(r => io.once('line',r))
+  }
+
+  while(true) {
+    const line = await getLine();
+    if (line.length === 0) {
+      io.close();
+      break;
+    }
+    run(line);
+  }
+}
+
+function run(input: string) {
+  console.log(input)
+}
+
+await main();
