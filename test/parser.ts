ca4899a270b10d9fbf1c08717a54f7c72bd44769
Miika Leinonen <miika@uprightproject.com>
2024-10-27 20:20:32 +0200

    Add minimal test for parser

---
+++ test/parser.ts
@@ -0,0 +1,19 @@
+import { assert } from "chai";
+import { Parser } from "../src/parser";
+import { TokenType } from "../src/token";
+
+describe("parser", function () {
+  it("Should parse expressions", function () {
+    const expr = new Parser([
+      { tokenType: TokenType.BANG, lexeme: "!", literal: null, line: 1 },
+      {
+        tokenType: TokenType.STRING,
+        line: 1,
+        lexeme: '"juuh"',
+        literal: "juuh",
+      },
+      { tokenType: TokenType.EOF, line: 2, lexeme: "", literal: "" },
+    ]).parse();
+    assert.isNotNull(expr);
+  });
+});
